4ce9137221e075cb894fd64152d9642be4817117
mahirs0309 <41713795+mahirs0309@users.noreply.github.com>
2018-08-14 17:01:53 -0400

    create github pages

---
+++ members-home.php
@@ -0,0 +1,202 @@
+<?php session_start();?><!DOCTYPE html>
+<html >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.8.1, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.8.1, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo2.png" type="image/x-icon">
+  <meta name="description" content="Website Creator Description">
+  <title>Members Home Page</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  <meta name="author" content="Steve Riches">
+<!--MobiMembers System | from RichoSoft | - For Mobirise Sites-->
+<!-- Created by Steve Riches - October 2016 -->
+<!-- For support contact support@richosoft.co.uk -->
+<!-- (C) 2016 - All Rights Reserved - RichoSoft - www.richosoft.co.uk -->
+<link rel="stylesheet" href="css/RSinvisible.css" type="text/css">
+<script src="js/sweetalert.min.js"></script>
+<link rel="stylesheet" href="css/sweetalert.css">
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+  <section class="menu cid-qZ2aDeQbgG" once="menu" id="menu1-1j">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm bg-color transparent">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                
+                
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown navbar-nav-top-padding" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="https://mobirise.com">
+                        </a>
+                </li><li class="nav-item"><a class="nav-link link text-white display-4" href="https://mobirise.com">
+                        </a></li><li class="nav-item"><a class="nav-link link text-white display-4" href="https://mobirise.com">
+                        </a></li><li class="nav-item"><a class="nav-link link text-white display-4" href="index.html">
+                        <span class="mbri-home mbr-iconfont mbr-iconfont-btn"></span>Home</a></li><li class="nav-item dropdown open"><a class="nav-link link text-white dropdown-toggle display-4" href="https://fhsmun.com/members-home.php" data-toggle="dropdown-submenu" aria-expanded="true"><span class="mbri-globe-2 mbr-iconfont mbr-iconfont-btn"></span>
+                        Delegate Hub</a><div class="dropdown-menu"><a class="text-white dropdown-item display-4" href="index.html#footer6-1e">Calendar</a><a class="text-white dropdown-item display-4" href="index.html#footer6-1e" aria-expanded="false">Resources</a><a class="text-white dropdown-item display-4" href="https://fhsmun.com/suggestions" aria-expanded="false">Suggestions</a><a class="text-white dropdown-item display-4" href="index.html#content9-13" aria-expanded="false">Upcoming Events</a></div></li><li class="nav-item dropdown">
+                    <a class="nav-link link text-white dropdown-toggle display-4" href="https://mobirise.com" data-toggle="dropdown-submenu" aria-expanded="false">
+                        <span class="mbri-search mbr-iconfont mbr-iconfont-btn"></span>
+                        About Us
+                    </a><div class="dropdown-menu"><a class="text-white dropdown-item display-4" href="index.html#content4-1b">Our Organization<br></a><a class="text-white dropdown-item display-4" href="index.html#features16-m" aria-expanded="false">Meet The E-Board<br></a><a class="text-white dropdown-item display-4" href="index.html#table1-q" aria-expanded="false">Delegation Awards</a><a class="text-white dropdown-item display-4" href="index.html#content4-u" aria-expanded="false">Our Legacy</a></div>
+                </li><li class="nav-item dropdown"><a class="nav-link link text-white dropdown-toggle display-4" href="https://mobirise.com" data-toggle="dropdown-submenu" aria-expanded="false"><span class="mbri-star mbr-iconfont mbr-iconfont-btn"></span>
+                        FHSMUNC</a><div class="dropdown-menu"><a class="text-white dropdown-item display-4" href="index.html#footer1-11">Registration</a><a class="text-white dropdown-item display-4" href="index.html#footer1-11" aria-expanded="false">Conference Information</a><a class="text-white dropdown-item display-4" href="index.html#footer1-11" aria-expanded="false">Middle School</a></div></li></ul>
+            <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-primary display-7" href="https://mobirise.com">Contact Us
+                    </a></div>
+        </div>
+    </nav>
+</section>
+
+<section class="engine"><a href="https://mobiri.se/q">free responsive website templates</a></section><section class="mbr-section content4 cid-r0JC1KU8Aa" id="content4-1m">
+
+    
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="title col-12 col-md-8">
+                <h2 class="align-center pb-3 mbr-fonts-style display-2">
+                    Delegate Hub</h2>
+                <h3 class="mbr-section-subtitle align-center mbr-light mbr-fonts-style display-5">
+                    Welcome to the Delegate Hub! &nbsp;Please log in to access the Delegate Hub.</h3>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section once="" class="cid-r0apPW5Zjv" id="footer6-1k">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="col-12">
+                <p class="mbr-text mb-0 mbr-fonts-style display-7">
+                    Delegate Calendar and Resources Coming Soon</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="cid-r0JBY7wm4w" id="footer1-1l">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row content text-white">
+            <div class="col-12 col-md-3">
+                <div class="media-wrap">
+                    <a href="https://mobirise.com/">
+                        <img src="assets/images/mbr-326x217.jpg" alt="Mobirise" title="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Address
+                </h5>
+                <p class="mbr-text">500 Elizabeth Ave, Somerset, NJ 08873</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Contacts
+                </h5>
+                <p class="mbr-text">
+                    Email: delegate.development@gmail.com &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<br>Phone: (732) 302-4200</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Links
+                </h5>
+                <p class="mbr-text">
+                    <a class="text-primary" href="https://www.franklinboe.org">Franklin Township&nbsp;</a><br><br></p>
+            </div>
+        </div>
+        <div class="footer-lower">
+            <div class="media-container-row">
+                <div class="col-sm-12">
+                    <hr>
+                </div>
+            </div>
+            <div class="media-container-row mbr-white">
+                <div class="col-sm-6 copyright">
+                    <p class="mbr-text mbr-fonts-style display-7">Website Developed by Mahir SIngh</p>
+                </div>
+                <div class="col-md-6">
+                    <div class="social-list align-right">
+                        <div class="soc-item">
+                            <a href="https://instagram.com/fhsmun" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.facebook.com/pages/Mobirise/1616226671953247" target="_blank">
+                                <span class="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                            </a>
+                        </div>
+                        
+                        
+                        
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  <script type="text/javascript">
+$( "#cbpassword" ).on('click',function(event) {
+$(this).toggleClass('toggled');
+  if($(this).hasClass('toggled')){document.getElementById('cpassword').style.visibility='visible';
+document.getElementById('cpassword').style.display='inline-block';
+}else{
+  document.getElementById('cpassword').style.visibility='hidden';document.getElementById('cpassword').style.display='none';
+}
+})
+</script>
+<script src="js/RSchangepass.js"></script>
+</body>
+</html>
